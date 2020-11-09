3389a27b5fd479e1b106aa6830d5799c4901a7cb
teneons <teneons@gmail.com>
2020-11-09 21:12:23 +0200

    add - add btn; corrected size icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,18 @@ import logo from './logo.svg';
 
 export default function Header() {
 
+  const svgPlus = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" class="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+    </svg>
+
   return (
     <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
       <span className="navbar-brand text-uppercase text-white font-weight-bold" href="#">
         <img src={logo} width="35" height="35" class="d-inline-block align-top" alt="" loading="lazy"></img>
           Product list
       </span>
+      <button type="button" class="btn btn-light ml-5">{svgPlus}</button>
     </nav>
   )
 }
