37a3d497ca1db16090efc19600495b3d8c79f940
teneons <teneons@gmail.com>
2020-11-11 19:26:55 +0200

    add search ui

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,14 +19,21 @@ class Header extends Component {
       <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
     </svg>
 
+    const svgSearch = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+      <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+    </svg>
+
     return (
       <div>
         <nav className="navbar navbar-light bg-dark d-flex justify-content-center">
           <span className="navbar-brand text-uppercase text-white font-weight-bold" href="#">
             <img src={logo} width="35" height="35" className="d-inline-block align-top" alt="" loading="lazy"></img>
-          Product list <span className="badge bg-light text-dark">PRO</span>
-      </span>
-          <button type="button" className="btn btn-light ml-5" data-toggle="modal" data-target="#exampleModal">{svgPlus}</button>
+            Product list <span className="badge bg-light text-dark">PRO</span>
+          </span>
+            <button type="button" className="btn btn-light ml-5" data-toggle="modal" data-target="#exampleModal">{svgPlus}</button>
+            <input type="text" className="ml-4" ref={(input) => this.txtSearch = input} placeholder="Search proructs" />
+            <button type="button" className="btn btn-light input-group-text">{svgSearch}</button>
         </nav>
 
         <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
