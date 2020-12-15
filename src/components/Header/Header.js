2dc504b2012198e1885fa3b0de857767fab7c82c
teneons <teneons@gmail.com>
2020-12-15 20:31:51 +0200

    from clss to func, use hooks

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,38 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import {connect} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-class Header extends Component {
+const Header = () => {
 
-  allDataProduct() {
-    this.props.headerAddData(Math.random().toString(36).substr(2, 9), this.txtTitle.value, this.txtDescription.value, this.txtPrice.value, this.txtImg.value, false)
-    this.txtTitle.value = ''
-    this.txtDescription.value = ''
-    this.txtPrice.value = ''
-    this.txtImg.value = ''
+  //add new product
+  const [inputProduct, setProductData] = useState({name: '', description: '', price: '', img: ''})
+  const getNewProd = e => {
+    setProductData({...inputProduct, [e.target.name]: e.target.value})
   }
 
-  getSearchTxt() {
-    this.props.searchData(this.txtSearch.value)
+  const setToRedux = () => {
+    this.headerAddData(inputProduct)
+  }
+
+  const getSearchTxt = () => {
+    //this.props.searchData(this.txtSearch.value)
   }
   
-  render() {
-    const svgPlus = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-      <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-    </svg>
+  //svg
+  const svgPlus = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-plus-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+    <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+  </svg>
 
-    const svgSearch = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-      <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-      <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-    </svg>
+  const svgSearch = <svg width="1.2em" height="1.2em" viewBox="0 0 16 16" className="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+    <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+  </svg>
 
-    const svgToggler = <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" fill="currentColor" className="bi bi-bookshelf" viewBox="0 0 16 16">
-      <path fillRule="evenodd" d="M2.5 0a.5.5 0 0 1 .5.5V2h10V.5a.5.5 0 0 1 1 0v15a.5.5 0 0 1-1 0V15H3v.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zM3 14h10v-3H3v3zm0-4h10V7H3v3zm0-4h10V3H3v3z"/>
-    </svg>
+  const svgToggler = <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" fill="currentColor" className="bi bi-bookshelf" viewBox="0 0 16 16">
+    <path fillRule="evenodd" d="M2.5 0a.5.5 0 0 1 .5.5V2h10V.5a.5.5 0 0 1 1 0v15a.5.5 0 0 1-1 0V15H3v.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 .5-.5zM3 14h10v-3H3v3zm0-4h10V7H3v3zm0-4h10V3H3v3z"/>
+  </svg>
 
+console.log(inputProduct)
     return (
       <div>
         <nav className="d-flex justify-content-center navbar navbar-expand-lg bg-dark">
@@ -51,8 +54,8 @@ class Header extends Component {
               <div className="d-flex justify-content-around container">
                 <button type="button" className="btn btn-light" data-bs-toggle="modal" data-bs-target="#staticBackdrop">{svgPlus}</button>
                 <div className="d-flex align-items-center">
-                  <input type="search" className="form-control mr-1" ref={(input) => this.txtSearch = input} placeholder="Search product" aria-label="Search" />
-                  <button type="button" onClick={this.getSearchTxt.bind(this)} className="btn btn-light">{svgSearch}</button>
+                  <input type="search" className="form-control mr-1" placeholder="Search product" aria-label="Search" />
+                  <button type="button" onClick={getSearchTxt.bind(this)} className="btn btn-light">{svgSearch}</button>
                 </div>
               </div>
             </div>
@@ -69,12 +72,12 @@ class Header extends Component {
               </div>
               <div className="modal-body">
                   <div>
-                    <input type="text" className="form-control mb-2" ref={(input)=> this.txtTitle = input} placeholder="Product name" required/>
-                    <input type="text" className="form-control mb-2" ref={(input)=> this.txtDescription = input}  placeholder="Product description" required />
-                    <input type="number" className="form-control mb-2" ref={(input)=> this.txtPrice = input} placeholder="Product price" required/>
-                    <input type="text" className="form-control mb-2" ref={(input)=> this.txtImg = input} placeholder="Product url img" required/>
+                    <input type="text" className="form-control mb-2" onChange={getNewProd} name='name' placeholder="Product name" required/>
+                    <input type="text" className="form-control mb-2" onChange={getNewProd} name='description' placeholder="Product description" required />
+                    <input type="number" className="form-control mb-2" onChange={getNewProd} name='price' placeholder="Product price" required/>
+                    <input type="text" className="form-control mb-2" onChange={getNewProd} name='img' placeholder="Product url img" required/>
                     <div className="d-flex justify-content-center">
-                      <button type="button" onClick={this.allDataProduct.bind(this)} className="btn btn-dark">Add {svgPlus}</button>
+                      <button type="button" onClick={setToRedux} className="btn btn-dark">Add {svgPlus}</button>
                     </div>
                   </div>
               </div>
@@ -83,16 +86,14 @@ class Header extends Component {
         </div>
         
       </div>
-
-    )
-  }
+  )
 }
 
 export default connect(
   state => ({}),
   dispatch => ({
-    headerAddData: (id, title, description, price, img, top) => {
-      dispatch({type: 'ADD_PRODUCT', payload: {id: id, title: title, description: description, price: price, img: img, top: top}})
+    headerAddData: (id = Math.random().toString(36).substr(2, 9), title, description, price, img) => {
+      dispatch({type: 'ADD_PRODUCT', payload: {id: id, title: title, description: description, price: price, img: img}})
   },
     searchData: (searchTxt)=>{
       dispatch({type: 'SEARCH_PRODUCT', payload: searchTxt})
